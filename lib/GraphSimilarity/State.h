9927b6a5c99b8e39bfaaefc05c8b709c0b8e6af7
fjvxmap <pcl412@snu.ac.kr>
2024-08-23 00:07:45 +0900

    .

--- lib/GraphSimilarity/State.h
+++ lib/GraphSimilarity/State.h
@@ -2,6 +2,15 @@
 #include "DataStructure/LabeledGraph.h"
 
 static int NumG1Vertices = 0, NumG2Vertices = 0;
-struct State {};
+struct State {
+  State *parent = NULL;
+  int id = 0, next_mapping_order = -1;
+  int cost = 0, depth = -1, lower_bound = 0;
+  int vertex_label_bound = 0, inner_edge_label_bound = 0,
+      cross_edge_label_bound = 0;
+  int *mapping;
+  int *inverse_mapping;
+  static int global_state_id;
+};
 
 int State::global_state_id = 0;
